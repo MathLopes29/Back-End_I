9a3f7fa4e29776a48020c071f4b83fd1fd962812
Matheus Lopes <98532951+MathLopes29@users.noreply.github.com>
2022-10-21 00:09:38 -0300

    Create 07_Operadores Simples.java

---
+++ Arquivos Txt/Java/07_Operadores Simples.java
@@ -0,0 +1,25 @@
+/** 
+* Aula 13 - Operadores
+* Operadores Java part.1
+* @author Matheus Lopes 
+*/
+
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+
+public class Calculo{
+	
+	public static void main (String[] args){
+	
+	// 2 + 3 Operador Binário //
+	// -2 Operador Unário //
+	// true ? "sim" : "não" Operadores Ternário //
+	
+	boolean x = 9 > 4;
+	System.out.println(x);
+
+	double y = (7 - 4 * 2) + 5;
+	System.out.println(y);
+	
+	}
+}
