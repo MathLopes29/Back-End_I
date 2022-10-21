fcda34e9cd177c0d8e099a63690a9edf62e82228
Matheus Lopes <98532951+MathLopes29@users.noreply.github.com>
2022-10-21 00:06:52 -0300

    Create 03_Modificadores.java

---
+++ Arquivos Txt/Java/03_Modificadores.java
@@ -0,0 +1,30 @@
+/**
+* Aula 009 - Contantes e Modificadores
+* Definição e estudo de Modificadores Finais
+* Criação de Variáveis que não podem ser modificados
+* @author Matheus Lopes
+*/
+
+public class ConstMod {
+	
+	public static void main (String[] args){
+	
+	// Valor que se modifica a todo momento //
+	int populacaoBr = 203429773;
+	populacaoBr = 203429774;
+	
+	// Valor que não se altera! //
+	final double PI = 3.14;
+	final char = SEXO_MAC = 'M';
+	final char = SEXO_FEM = 'F';
+	
+	}
+
+/**
+* Na 'populacaoBr' consigo modificar a todo momento nosso valor
+* Enquanto na 'final double' - se tornou uma constante que não pode mais ser modificada
+* Por ser um final mod sua nomeclatura é diferente !
+*/
+	
+}
+
