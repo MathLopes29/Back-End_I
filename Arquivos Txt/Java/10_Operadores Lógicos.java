fe4ec2e58a464cbf9b9d2cbb5df6e2d2af6fe757
Matheus Lopes <98532951+MathLopes29@users.noreply.github.com>
2022-10-21 00:11:53 -0300

    Create 10_Operadores Lógicos.java

---
+++ Arquivos Txt/Java/10_Operadores Lógicos.java
@@ -0,0 +1,29 @@
+/** 
+* Aula 16 - Operadores Lógicos
+* Lógica
+* @author Matheus Lopes
+*/
+
+import java.util.Scanner;
+
+public class Logica {
+	
+	public static void main (String[] args){
+		
+		int x = 6;
+		System.out.println((x >= 6) && (x >= 5));
+		
+		int y = 10;
+		System.out.println((y >= 2) || (y >= 15));
+		
+		int z = 9;
+		System.out.println(!(x < 11));
+	}
+}
+
+/** OPERADORES LÓGICOS
+
+* && AND
+* || OR
+* !  NOT
+*/
