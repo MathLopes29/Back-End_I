b16a8d27381f464b5bfc1e3412845494cd15ad5d
Matheus Lopes <98532951+MathLopes29@users.noreply.github.com>
2022-10-21 00:04:03 -0300

    Update and rename 005_Fundamentos em Java.java to Fundamentos em Java.java

---
+++ Arquivos Txt/Java/Fundamentos em Java.java
@@ -0,0 +1,25 @@
+/** 
+* Java XTI
+* Aula 005 - Fundamentos em Java
+* @author Matheus Lopes
+*/
+
+public class Script_2 {	
+	/* Método principal da classe */
+	public static void main (String[] args)/* Parte não obrigatória - throws Exception*/{
+		// printf e cout em java //
+		System.out.println("Write once and run everywhere!");
+		System.out.println("\tHello World! I'm back !\n");
+		System.out.println("\tHere we go!\n");
+		System.out.println("\tOlá Andarílho!\n");
+		System.out.println("\tJava \nSE");
+	} // Fim do método //
+} // Fim da classe //
+
+
+/** 
+* Converção de padrões de códigos Java - Regra Padrão 
+* Java Keywords ! - Palavras reservadas à linguagem Java
+* Caracter de Escape -  \n e \t
+* (...) = ("\Olá\" Andarílho!\"") - Significa que teremos áspas no println
+*/
