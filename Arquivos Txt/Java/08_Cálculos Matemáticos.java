086948a357fea46a3d2d07e776f4588e201f8abf
Matheus Lopes <98532951+MathLopes29@users.noreply.github.com>
2022-10-21 00:10:26 -0300

    Create 08_Cálculos Matemáticos.java

---
+++ Arquivos Txt/Java/08_Cálculos Matemáticos.java
@@ -0,0 +1,27 @@
+/** 
+* Aula 14 - Cálculos Matemáticos
+* Operadores Java 
+* @author Matheus Lopes
+*/
+
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+
+public class Operador{
+
+	public static void main (String[] args){
+		
+		//double x = 6 % 3;
+		//double y = -(+5);
+		
+		// String y = "Oi" + "Programador Java";
+		
+		int x = 6;
+		int y = x++;   // x = x + 1; //
+		// int y = x--;
+		
+		System.out.println("x=" + x);
+		System.out.println("y=" + y);
+	}
+	
+}
