758551dbc55bf664f48aca76c2bfa01aaa68a782
Matheus Lopes <98532951+MathLopes29@users.noreply.github.com>
2022-10-24 22:21:36 -0300

    Create C++ Taboada.cpp

---
+++ Arquivos Txt/C++/C++ Taboada.cpp
@@ -0,0 +1,19 @@
+#include "iostream"
+#include "math.h"
+#include "cstdlib"
+
+using namespace std;	
+int main(){
+
+int N1,R;
+
+cout<<"Digite o valor: "<<endl;  
+cin>>N1;
+cout<<endl;
+
+for(int i=1; i<11; i++){
+cout<<N1*i<<endl;
+}	
+	exit (0);
+	return 0;
+}
